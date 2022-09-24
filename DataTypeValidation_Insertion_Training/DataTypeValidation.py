b83ca59f169c52aaf4a5f7c1a6256edca949445e
deepakthakur-92 <dt9971678995@gmail.com>
2022-09-24 17:07:35 +0530

    modified datatypevalidation file

--- DataTypeValidation_Insertion_Training/DataTypeValidation.py
+++ DataTypeValidation_Insertion_Training/DataTypeValidation.py
@@ -11,7 +11,7 @@ class dBOperation:
     """
       This class shall be used for handling all the SQL operations.
 
-      Written By: iNeuron Intelligence
+      Written By: Deepak Thakur
       Version: 1.0
       Revisions: None
 
@@ -31,7 +31,7 @@ class dBOperation:
                 Output: Connection to the DB
                 On Failure: Raise ConnectionError
 
-                 Written By: iNeuron Intelligence
+                 Written By: Deepak Thakur
                 Version: 1.0
                 Revisions: None
 
@@ -56,7 +56,7 @@ class dBOperation:
                         Output: None
                         On Failure: Raise Exception
 
-                         Written By: iNeuron Intelligence
+                         Written By: Deepak Thakur
                         Version: 1.0
                         Revisions: None
 
@@ -119,7 +119,7 @@ class dBOperation:
                                Output: None
                                On Failure: Raise Exception
 
-                                Written By: iNeuron Intelligence
+                                Written By: Deepak Thakur
                                Version: 1.0
                                Revisions: None
 
@@ -167,7 +167,7 @@ class dBOperation:
                                Output: None
                                On Failure: Raise Exception
 
-                                Written By: iNeuron Intelligence
+                                Written By: Deepak Thakur
                                Version: 1.0
                                Revisions: None
 
